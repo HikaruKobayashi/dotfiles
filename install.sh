c566d1b45e9e60fb6cc83c17cc581a84e241ed38
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-03-29 15:34:46 +0900

    vscode path was wrong.

--- install.sh
+++ install.sh
@@ -4,20 +4,19 @@ set -ue
 # vscode
 echo "vscode"
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
 if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
   rm "${VSCODE_SETTING_DIR}/settings.json"
 fi
-ln -s "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+ln -s ~/dotfiles/vscode/settings.json "${VSCODE_SETTING_DIR}/settings.json"
 
 if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
   rm "${VSCODE_SETTING_DIR}/keybindings.json"
 fi
-ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+ln -s ~/dotfiles/vscode/keybindings.json "${VSCODE_SETTING_DIR}/keybindings.json"
 
-cat < ./extensions | while read -r line
+cat < ./vscode/extensions | while read -r line
 do
   code --install-extension "$line"
 done
